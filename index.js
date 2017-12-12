07288ad8ca84c74366fb7ce53721b7a1ed65e906
Jeff Goodall <goodallhj@gmail.com>
2017-12-12 10:46:17 -0700

    added generate random letters

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const generateRandomNumbers = require('./lib/random-numbers.js');
+const generateRandomLetters = require('./lib/random-letter.js');
 
 module.exports = {
   generateRandomNumbers
+  generateRandomLetters
 }
